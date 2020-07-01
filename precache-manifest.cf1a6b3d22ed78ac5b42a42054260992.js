087d94057582b1a78c1605106a0eda264ef25bc1
RakshithRAcharya <rakshithacharya1030@gmail.com>
2020-07-01 23:21:03 +0530

    Updates

---
+++ precache-manifest.cf1a6b3d22ed78ac5b42a42054260992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef38d89b3bb555bef3467a0787b6d6a",
+    "url": "/React-Recipe/index.html"
+  },
+  {
+    "revision": "84182efe829750be2b71",
+    "url": "/React-Recipe/static/css/main.9bd58b56.chunk.css"
+  },
+  {
+    "revision": "80878e45ed6319cce9b0",
+    "url": "/React-Recipe/static/js/2.b499d4fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe/static/js/2.b499d4fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84182efe829750be2b71",
+    "url": "/React-Recipe/static/js/main.120e5f8f.chunk.js"
+  },
+  {
+    "revision": "a94ae2bcbf82c9060ebd",
+    "url": "/React-Recipe/static/js/runtime-main.a568c383.js"
+  }
+]);
